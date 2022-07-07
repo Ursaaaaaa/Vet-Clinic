eb783a8e774c3a705e3f7ab75ebb75e20417fdd3
ramrod433 <djoletoamanor@gmail.com>
2022-07-07 19:27:27 +0000

    Added queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,11 @@ select name, escape_attempts from animals where weight_kg > 10.5;
 select * from animals where neutered = true;
 select * from animals where name not in ('Gabumon');
 select * from animals where weight_kg >=10.4 and weight_kg <=17.3;
+
+
+select count(*) from animals;
+select count(*) from animals where escape_attempts = 0;
+select avg(weight_kg) from animals;
+select neutered, max(escape_attempts) from animals group by neutered;
+select species, max(weight_kg), min(weight_kg) from animals group by species;
+select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species;
