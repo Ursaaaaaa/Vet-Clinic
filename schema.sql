0bd43a96581ec6d53c7ee059e6688a74a7019713
ramrod433 <djoletoamanor@gmail.com>
2022-07-07 23:31:36 +0000

    Added queries

--- schema.sql
+++ schema.sql
@@ -5,4 +5,13 @@ alter table animals add column species VARCHAR(100);
 CREATE TABLE owners ( id serial PRIMARY KEY, full_name VARCHAR(100) NOT NULL, age INT NOT NULL);
 CREATE TABLE species ( id serial PRIMARY KEY, name VARCHAR(100) NOT NULL);
 
- alter table animals add PRIMARY KEY (id);
+alter table animals add PRIMARY KEY (id);
+
+alter table animals drop column species;
+alter table animals add species_id INT;
+alter table animals add owner_id INT;
+
+
+
+alter table animals add constraint fk_species_table FOREIGN KEY(species_id) references species(id);
+alter table animals add constraint fk_owner_table FOREIGN KEY(owner_id) references owners(id);
